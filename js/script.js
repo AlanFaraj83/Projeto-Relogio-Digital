1fea404752b963dfb3e05465f66ebda9425cdea3
AlanFaraj83 <alanfaraj@outlook.com>
2023-04-30 21:48:54 -0300

    :sparkles: Atualizando  o script.js

--- js/script.js
+++ js/script.js
@@ -3,5 +3,8 @@ const minutos = document.getElementById('minutos');
 const secundos = document.getElementById('secundos');
 
 const relogio = setInterval(function time() {
-
+    let dateToday = new Date();
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let sec = dateToday.getSeconds();
 })
